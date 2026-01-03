0068c423e9c158229f306226e86ce88a8ba76102
yami <ravirrajpatil@gmail.com>
2026-01-03 20:00:23 +0530

    feat(accept-message): refactor dbConnect calls and add error handling for GET route

--- app/api/accept-message/route.ts
+++ app/api/accept-message/route.ts
@@ -4,6 +4,8 @@ import { dbConnect } from "@/lib/dbConnect";
 import UserModel from "@/models/User.models";
 
 export async function POST(request: Request) {
+  dbConnect();
+
   const session = await getServerSession(authOptions);
   if (!session || !session.user?.email) {
     return Response.json(
@@ -14,8 +16,6 @@ export async function POST(request: Request) {
   const userId = session.user._id;
 
   try {
-    dbConnect();
-
     const { acceptingMessage } = await request.json();
 
     const updatedUser = await UserModel.findByIdAndUpdate(
@@ -46,6 +46,48 @@ export async function POST(request: Request) {
         message: "someting went wrong while toggeling the accept message",
       },
       { status: 400 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  dbConnect();
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!session || !session?.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      { status: 401 }
+    );
+  }
+  const userId = user?._id;
+  try {
+    const existingUser =
+      await UserModel.findById(userId).select("isAcceptingMessage");
+    if (!existingUser) {
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 404 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessage: existingUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return Response.json(
+      {
+        success: false,
+        message: "Somthing went wrong while fetching the accept message state",
+      },
+      { status: 500 }
     );
   }
 }
