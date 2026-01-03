4eadbe344ff5901f83e3bd0ebc356e7b9582a18a
yami <ravirrajpatil@gmail.com>
2026-01-03 19:32:32 +0530

    feat(verification): implement POST route for verifying user accounts with verification code

---
+++ app/api/verify-code/route.ts
@@ -0,0 +1,53 @@
+import UserModel from "@/models/User.models";
+import { dbConnect } from "@/lib/dbConnect";
+import { z } from "zod";
+import { verifySchema } from "@/schemas/verifySchema";
+import { ca } from "zod/locales";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 400 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        { success: true, message: "Accound verified successfully" },
+        { status: 200 }
+      );
+    } else if (isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message: "Verification code is expired please sign in again",
+        },
+        { status: 400 }
+      );
+    } else {
+      return Response.json(
+        { success: false, message: "incorrect verification Code " },
+        { status: 400 }
+      );
+    }
+  } catch (er) {
+    console.log("Error while verifying the code ", er);
+    return Response.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
